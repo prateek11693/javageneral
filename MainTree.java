7a53907437788d29fcf8221819071717ac2d110d
prategup30 <prateek.gupta@meridianlink.com>
2020-08-08 17:13:50 +0530

    tree

---
+++ MainTree.java
@@ -0,0 +1,19 @@
+package com.general.general;
+
+public class MainTree {
+
+	public static void main(String[] args) {
+		
+		Tree tree = new Tree();
+		tree.insert(1);
+		tree.insert(4);
+		tree.insert(7);
+		tree.insert(8);
+		tree.insert(9);
+		tree.insert(10);
+		tree.insert(6);
+		tree.insert(8);
+
+	}
+
+}
---
+++ Tree.java
@@ -0,0 +1,46 @@
+package com.general.general;
+
+public class Tree {
+
+	private class Node {
+
+		private int value;
+		private Node leftChild;
+		private Node rightChild;
+
+		public Node(int value) {
+			this.value = value;
+		}
+
+	}
+
+	private Node root;
+
+	public void insert(int value) {
+		if (root == null) {
+			root = new Node(value);
+
+			return;
+		}
+
+		Node current = root;
+
+		while (true) {
+			if (value < current.value) {
+				if (current.leftChild == null) {
+					current.leftChild = new Node(value);
+					return;
+				}
+				current = current.leftChild;
+			} else {
+				if (current.rightChild == null) {
+					current.rightChild = new Node(value);
+					return;
+				}
+				current = current.rightChild;
+			}
+		}
+
+	}
+
+}
